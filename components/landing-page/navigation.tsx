feb53de20ea461877963962e71baa8236685be61
Oye Olalekan Johnson <johnsonoye34@gmail.com>
2024-07-21 08:49:08 +0100

    chore: change navbar to the logo.

--- components/landing-page/navigation.tsx
+++ components/landing-page/navigation.tsx
@@ -9,7 +9,7 @@ export function Navigation() {
     <nav className="sticky z-50 inset-x-0 top-0 py-3 border-b backdrop-blur-sm px-2 dark:border-gray-800">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-1">
         <a href="#" className="flex items-center space-x-1 rtl:space-x-reverse">
-          <span className="self-center font-semibold whitespace-nowrap uppercase dark:text-gray-300">App Forest</span>
+          <span className="self-center font-semibold whitespace-nowrap uppercase dark:text-gray-300">APPSTATE</span>
         </a>
         <div className='space-x-3'>
           <Button variant={'outline'} size={'small'} onClick={() => router.push("/auth/sign-in")}>Sign In</Button>
