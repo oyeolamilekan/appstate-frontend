b2df96020ca88bfaf7b2dd64dc8963b1750778d9
Oye Olalekan Johnson <johnsonoye34@gmail.com>
2024-08-13 11:02:10 +0100

    fix: fix logo issue.

--- components/ui/logo.tsx
+++ components/ui/logo.tsx
@@ -1,13 +1,23 @@
-import React from 'react'
+import React from 'react';
 import Image from 'next/image';
 import { useTheme } from 'next-themes';
 
 export function Logo() {
-  const { theme } = useTheme();
+  const { resolvedTheme } = useTheme();
+
+  const logoSrc = resolvedTheme === 'dark' ? '/logo/LogoWhite.png' : '/logo/LogoBlack.png';
 
   return (
-    <div className="p-[0.1rem] rounded-full">
-      <Image src={theme?.toLowerCase() === 'dark' || theme === null ? `/logo/LogoWhite.png` : `/logo/LogoBlack.png`} alt='Logo' width={40} height={40} className='rounded-full'/>
+    <div className="p-[0.1rem] rounded-full bg-gray-200">
+      <Image 
+        src={logoSrc} 
+        alt='Appstate Logo' 
+        width={40} 
+        height={40} 
+        className='rounded-full'
+        priority
+        onError={() => console.error('Failed to load image:', logoSrc)}
+      />
     </div>
-  )
+  );
 }
